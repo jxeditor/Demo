bfd37f3d1f59823ddac2aef1b35ad0b608ebdd85
jxeditor <980813351@qq.com>
2019-12-27 15:41:40 +0800

    测试Demo

---
+++ src/main/java/com/test/algorithm/leetcode/q0001t0100/q2/Question0012.java
@@ -0,0 +1,22 @@
+package com.test.algorithm.leetcode.q0001t0100.q2;
+
+/**
+ * @Author: xs
+ * @Date: 2019-12-27 15:39
+ * @Description: 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ */
+public class Question0012 {
+    public static void main(String[] args) {
+
+    }
+}
